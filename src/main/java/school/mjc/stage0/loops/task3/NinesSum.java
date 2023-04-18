c8bc130701f30734eb45fb76a78f56e24ab9ce1b
d.anisimov <d.anisimov@ds-manager03.szspspb.local>
2023-04-18 20:14:24 +0300

    fixed.

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -8,7 +8,6 @@ public class NinesSum {
     		double k = 0;
     		for(int counter = 0; counter < lengthOfLastNumber; counter++ ) {
     			k = k + Math.pow(10, counter);
-    			System.out.println(k);
     		}
     		sum = sum + 9 * k;
     		lengthOfLastNumber--;
